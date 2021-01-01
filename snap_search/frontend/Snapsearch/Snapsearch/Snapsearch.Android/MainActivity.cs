d104e4077e49d0d902d463fffce6deb19dbcb8fe
sinantutan <sintedu1@gmail.com>
2021-01-01 14:16:39 +0100

    Fixed Camera & Gallery Picker

--- snap_search/frontend/Snapsearch/Snapsearch/Snapsearch.Android/MainActivity.cs
+++ snap_search/frontend/Snapsearch/Snapsearch/Snapsearch.Android/MainActivity.cs
@@ -1,11 +1,8 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace Snapsearch.Droid
 {
@@ -17,12 +14,21 @@ namespace Snapsearch.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
+            this.SetStatusBarColor(Xamarin.Forms.Color.Orange.ToAndroid());
 
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials nuget package
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+        /// <summary>
+        /// Xamarin.Essentials Nuget Package Permission Requests
+        /// </summary>
+        /// <param name="requestCode"></param>
+        /// <param name="permissions"></param>
+        /// <param name="grantResults"></param>
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- snap_search/frontend/Snapsearch/Snapsearch/Snapsearch/Views/ChoosingPhotoPage.xaml.cs
+++ snap_search/frontend/Snapsearch/Snapsearch/Snapsearch/Views/ChoosingPhotoPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,5 +17,31 @@ namespace Snapsearch.Views
             InitializeComponent();
         }
 
+        async void TakePhoto_OnClicked(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                PickedImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+        async void ImageButton_OnClicked(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Choose an image"
+                }
+            );
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                PickedImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
     }
 }
