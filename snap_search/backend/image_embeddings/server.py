4394a9bcc985296d1edfaa1c7cc4c8ae83c2cf26
Abdelrahman Mohamed <mohamed@heartbeat-med.de>
2021-01-20 22:25:04 +0100

    Try CI/CD on server

--- snap_search/backend/image_embeddings/server.py
+++ snap_search/backend/image_embeddings/server.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def test():
-    return 'Hello SnapSearch'
+    return 'Try the server CI/CD'
 
 @app.route('/uploadimage/<int:result_num>', methods=['POST'])
 def process_image(result_num):
