6aad1b3990dad9fa06feed564a592491978b6d7e
Abdelrahman Mohamed <mohamed@heartbeat-med.de>
2021-01-22 14:14:38 +0100

    test azure container reg

--- snap_search/backend/image_embeddings/server.py
+++ snap_search/backend/image_embeddings/server.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def test():
-    return 'Try from ci'
+    return 'This try should work'
 
 @app.route('/uploadimage/<int:result_num>', methods=['POST'])
 def process_image(result_num):
@@ -39,4 +39,4 @@ def create_records():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0" ,port=5000) 
+    app.run(debug=True, host="0.0.0.0" ,port=80) 
