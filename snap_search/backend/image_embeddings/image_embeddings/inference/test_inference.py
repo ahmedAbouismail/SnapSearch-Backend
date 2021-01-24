cae58d9c315080d4c5f10ad8e386cf2ad24a392d
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2021-01-24 18:20:05 +0100

    test

--- snap_search/backend/image_embeddings/image_embeddings/inference/test_inference.py
+++ snap_search/backend/image_embeddings/image_embeddings/inference/test_inference.py
@@ -42,9 +42,6 @@ class Test_Inference(unittest.TestCase):
         value = True
         checkValue = tf.train.Feature(int64_list=tf.train.Int64List(value = [value]))
         result = inference._int64_feature(value)
-        print("**********************test_int64_feature************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertEqual(result, checkValue)
         
@@ -59,9 +56,6 @@ class Test_Inference(unittest.TestCase):
         valueString = "string"
         checkValueString = tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(valueString, 'utf-8')]))
         resultString = inference._bytes_feature(bytes(valueString, 'utf-8'))
-        print("**********************test_bytes_feature************************")
-        print(resultString)
-        print("****************************************************************")
         self.assertIsNotNone(resultString)
         self.assertEqual(resultString, checkValueString)
         
@@ -69,9 +63,6 @@ class Test_Inference(unittest.TestCase):
         valuebyte = 16
         checkValueByte = tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(valuebyte)]))
         resultByte= inference._bytes_feature(bytes(valuebyte))
-        print("**********************test_bytes_feature************************")
-        print(resultByte)
-        print("****************************************************************")
         self.assertIsNotNone(resultByte)
         self.assertEqual(resultByte, checkValueByte)
         
@@ -86,9 +77,6 @@ class Test_Inference(unittest.TestCase):
          #tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(value)]))
         imageName = bytes("image_daisy_45", 'utf-8')
         result  = inference.serialize_example(image, imageName)
-        print("**********************test_serialize_example************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
 
     def test_tf_serialize_example(self):
@@ -102,9 +90,6 @@ class Test_Inference(unittest.TestCase):
         image = bytes(1)
         imageName = bytes("imageName", 'utf-8')
         result = inference.tf_serialize_example(image, imageName)
-        print("**********************test_tf_serialize_example************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         
         tfString = tf.py_function(inference.serialize_example, (image, imageName), tf.string)
@@ -121,12 +106,10 @@ class Test_Inference(unittest.TestCase):
 
         ***please-> put a path of one photo.jepeg as the example below  
         """
-        #path : where the images on my pc located 
-        path = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_images/image_daisy_45.jpeg"
+        #path : where the images on my pc located
+        path = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_images/image_daisy_45.jpeg"))
+        # path = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_images/image_daisy_45.jpeg"
         result = inference.process_path(path)
-        print("**********************test_process_path************************")
-        print(result[0].dtype)
-        print("****************************************************************")
         #test ervry element in tuple not None
         self.assertIsNotNone(result[0])
         self.assertIsNotNone(result[1])
@@ -143,13 +126,11 @@ class Test_Inference(unittest.TestCase):
 
         ***please-> put a path of one .tfrecords as the example below
         """
-        filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
+        filename = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_tf_records/part-001-db.tfrecord"))
+        # filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
         filenames = [filename]
         raw_dataset = tf.data.TFRecordDataset(filenames)
         result = raw_dataset.map(inference._parse_function)
-        print("**********************test_parse_function************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
 
@@ -164,13 +145,11 @@ class Test_Inference(unittest.TestCase):
         ***please-> put a path of one .tfrecords as the example below
         """
         #creat fake example_proto for the func
-        filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
+        filename = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_tf_records/part-001-db.tfrecord"))
+        # filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
         filenames = [filename]
         raw_dataset = tf.data.TFRecordDataset(filenames)
         result = raw_dataset.map(inference._parse_function).map(inference.preprocess_image)
-        print("**********************test_preprocess_image************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
         
@@ -185,12 +164,9 @@ class Test_Inference(unittest.TestCase):
 
         ***please-> put a path of one .tfrecords as the example below
         """
-
-        filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
+        filename = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_tf_records/part-001-db.tfrecord"))
+        # filename = "D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_tf_records/part-001-db.tfrecord"
         result = inference.read_tfrecord(filename)
-        print("**********************test_read_tfrecord************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
         
@@ -203,11 +179,10 @@ class Test_Inference(unittest.TestCase):
         """
         model = EfficientNetB0(weights="imagenet", include_top=False, pooling="avg")
         result = inference.tfrecords_to_write_embeddings(tf_output, embeddings_output, model,batch_size=1000, single_photo=False)
-        print("**********************test_tfrecords_to_write_embeddings************************")
-        print(result)
-        print("****************************************************************")
         for i in range(0,10):
-            path = ("D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_embeddings/part-00%s-db.parquet" %i)
+            # path = ("D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_embeddings/part-00%s-db.parquet" %i)
+            path = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_tf_records/part-00%s-db.tfrecord" %i))
+
             assert os.path.exists(path)
         
     def test_list_files(self):
@@ -223,9 +198,6 @@ class Test_Inference(unittest.TestCase):
 
         """
         result = inference.list_files(database_folder)
-        print("**********************test_list_files************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
 
@@ -243,9 +215,6 @@ class Test_Inference(unittest.TestCase):
         shard_list = list_ds.shard(10, 3)
         
         result = inference.read_data_from_files(shard_list)
-        print("**********************test_read_data_from_files************************")
-        print(result)
-        print("****************************************************************")
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
 
