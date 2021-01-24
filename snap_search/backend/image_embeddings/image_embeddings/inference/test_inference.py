1865957da2a09d735c2455262ac688c56db031d0
Abdelrahman Mohamed <mohamed@heartbeat-med.de>
2021-01-24 17:51:38 +0100

    Add test inferance to pipline

--- snap_search/backend/image_embeddings/image_embeddings/inference/test_inference.py
+++ snap_search/backend/image_embeddings/image_embeddings/inference/test_inference.py
@@ -13,18 +13,19 @@ import os
 """
 ***important:
 
-    1- make sure that you changed the paths to the paths where the files are saved on ur computer
-    2- run the test twice to get the right result
-        a- the first time to create the tf_output file and write the data
-        b- the seconde time to read the data from tf_output and write it in the embeddings_output
+    1- make sure that you changed the paths to the paths where the files are saved on ur computer.
+    2- run the test twice to get the right result.
+        a- the first time to create the tf_output file and write the data.
+        b- the seconde time to read the data from tf_output and write it in the embeddings_output.
 """
-tf_output = "backend/image_embeddings/system_files/tf_flower_tf_records"
-embeddings_output = "backend/image_embeddings/system_files/tf_flower_embeddings"
-database_folder = "backend/image_embeddings/system_files/tf_flower_images"
+currentDir = os.path.dirname(__file__)
+tf_output = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_tf_records"))
+embeddings_output = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_embeddings"))
+database_folder = os.path.abspath(os.path.join(currentDir, "../../system_files/tf_flower_images"))
 
 
 
-class TeastInference(unittest.TestCase):
+class Test_Inference(unittest.TestCase):
     # @classmethod
     
     def test_run_inference(self):
@@ -47,7 +48,6 @@ class TeastInference(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertEqual(result, checkValue)
         
-
     def test_bytes_feature(self):
         """
         test the function with two types String and Byte
@@ -75,7 +75,6 @@ class TeastInference(unittest.TestCase):
         self.assertIsNotNone(resultByte)
         self.assertEqual(resultByte, checkValueByte)
         
-
     def test_serialize_example(self):
         """
         pass fake image and image name to test the function
@@ -133,8 +132,7 @@ class TeastInference(unittest.TestCase):
         self.assertIsNotNone(result[1])
         self.assertIsInstance(result[0], tf.Tensor)
         self.assertIsInstance(result[1], tf.Tensor)
-
-    
+   
     def test_parse_function(self):
         """
         path .tfrecord to test the func
@@ -196,8 +194,6 @@ class TeastInference(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tf.data.Dataset)
         
-        
-    
     def test_tfrecords_to_write_embeddings(self):
         """
         pass the tfrecords to write embeddings
@@ -214,7 +210,6 @@ class TeastInference(unittest.TestCase):
             path = ("D:/HTW/Software Entwicklung/CBIR DB/gl/team9/snap_search/backend/image_embeddings/system_files/tf_flower_embeddings/part-00%s-db.parquet" %i)
             assert os.path.exists(path)
         
-
     def test_list_files(self):
         """
         path the database_folder to test the func
@@ -255,18 +250,28 @@ class TeastInference(unittest.TestCase):
         self.assertIsInstance(result, tf.data.Dataset)
 
         
-    
     def test_write_tfrecord(self):
-        """
-        pass the output_folder to write the tf files 
-
-        part_1: assert os.path.exists(path ->
-                check if the tfrecord has been written 
-        """
         inference.write_tfrecord(output_folder=tf_output, single_photo=False)
         
         assert os.path.exists(tf_output)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite=unittest.TestSuite()
+    suite.addTest(Test_Inference("test_run_inference"))
+    suite.addTest(Test_Inference("test_int64_feature"))
+    suite.addTest(Test_Inference("test_bytes_feature"))
+    suite.addTest(Test_Inference("test_serialize_example"))
+    suite.addTest(Test_Inference("test_tf_serialize_example"))
+    suite.addTest(Test_Inference("test_process_path"))
+    suite.addTest(Test_Inference("test_parse_function"))
+    suite.addTest(Test_Inference("test_preprocess_image"))
+    suite.addTest(Test_Inference("test_read_tfrecord"))
+    suite.addTest(Test_Inference("test_tfrecords_to_write_embeddings"))
+    suite.addTest(Test_Inference("test_list_files"))
+    suite.addTest(Test_Inference("test_read_data_from_files"))
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if result.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
